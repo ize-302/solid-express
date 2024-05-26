5b4bfd44d221da650b066372279cb7622be01bf5
ize-302 <adavizeozorku@gmail.com>
2024-05-26 20:55:46 +0100

    feat: improve ui

--- backend/controllers/socket.controller.js
+++ backend/controllers/socket.controller.js
@@ -9,33 +9,35 @@ class Socket {
     wss.on('connection', (ws, req) => {
       sessionMiddleware(req, {}, async function () {
         const { user: user_session_data } = req.session
-        console.log(`New websocket connection: ${user_session_data.username}`);
+        if (user_session_data) {
+          console.log(`New websocket connection: ${user_session_data?.username}`);
 
-        // get user detail
-        const userDetail = await fetchUserDetailByUsername(req, undefined, user_session_data.username)
-        ws.on('message', (message) => {
-          try {
-            const parsedMessage = JSON.parse(message)
-            const { type, content, channel, author } = parsedMessage
+          // get user detail
+          const userDetail = await fetchUserDetailByUsername(req, undefined, user_session_data.username)
+          ws.on('message', (message) => {
+            try {
+              const parsedMessage = JSON.parse(message)
+              const { type, content, channel, author } = parsedMessage
 
-            if (type === 'subscribe') {
-              // Subscribe to a channel
-              if (!clients.has(ws)) {
-                clients.set(ws, new Set());
-              }
-              clients.get(ws).add(channel);
-            } else if (type === 'message') {
-              // Broadcast the message to all clients
-              wss.clients.forEach((client) => {
-                if (client.readyState && clients.get(client)?.has(channel)) {
-                  client.send(JSON.stringify({ channel, content, author: userDetail }));
+              if (type === 'subscribe') {
+                // Subscribe to a channel
+                if (!clients.has(ws)) {
+                  clients.set(ws, new Set());
                 }
-              });
+                clients.get(ws).add(channel);
+              } else if (type === 'message') {
+                // Broadcast the message to all clients
+                wss.clients.forEach((client) => {
+                  if (client.readyState && clients.get(client)?.has(channel)) {
+                    client.send(JSON.stringify({ channel, content, author }));
+                  }
+                });
+              }
+            } catch (error) {
+              console.error('Error processing message', err);
             }
-          } catch (error) {
-            console.error('Error processing message', err);
-          }
-        });
+          });
+        }
 
         ws.on('close', () => {
           console.log('Client disconnected');
--- frontend/src/layouts/dashboard.jsx
+++ frontend/src/layouts/dashboard.jsx
@@ -1,10 +1,13 @@
-import { useNavigate } from "@solidjs/router";
+import { A, useLocation, useNavigate } from "@solidjs/router";
 import { AuthContext } from "../contexts/AuthContext";
-import { onMount, useContext } from "solid-js";
+import { For, onMount, useContext } from "solid-js";
+import logo from "../assets/logo.svg";
+import { Bell, Search, User, Chat, Group } from "../components/icons/MenuIcons";
 
 function DashboardLayout(props) {
   const { authenticated, checkSessionState } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   onMount(() => {
     checkSessionState().then(() => {
@@ -14,10 +17,66 @@ function DashboardLayout(props) {
     });
   });
 
+  const menus = [
+    {
+      name: "private chats",
+      icon: <Chat />,
+      path: "/",
+    },
+    // {
+    //   name: "group chat",
+    //   icon: <Group />,
+    //   path: "/group-chat",
+    // },
+    // {
+    //   name: "search",
+    //   icon: <Search />,
+    //   path: "/search",
+    // },
+    // {
+    //   name: "notification",
+    //   icon: <Bell />,
+    //   path: "/notifications",
+    // },
+  ];
+
   return (
-    <>
-      <div class="container mx-auto max-w-2xl">{props.children}</div>
-    </>
+    <div class="p-5 h-screen flex flex-col bg-green-50">
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div class="mt-4 overflow-hidden rounded-lg border border-black bg-background shadow h-full bg-white flex">
+        <div class="border-r border-black flex flex-col items-center justify-between px-3 pt-5">
+          <div>
+            <For each={menus}>
+              {(menu, i) => (
+                <A
+                  href={menu.path}
+                  class="text-2xl hover:bg-green-200 block p-2 rounded-lg mb-3 text-gray-800"
+                  classList={{
+                    "bg-green-200": location.pathname === menu.path,
+                  }}
+                >
+                  {menu.icon}
+                </A>
+              )}
+            </For>
+          </div>
+          {/* <A
+            href={`/profile`}
+            class="text-2xl hover:bg-green-200 inline-block p-2 rounded-lg mb-3 text-gray-800"
+          >
+            <User />
+          </A> */}
+        </div>
+        <div class="min-w-[400px] max-w-[400px] border-r border-black overflow-y-auto"></div>
+        <div class="bg-gray-100 w-full overflow-y-auto">{props.children}</div>
+      </div>
+      <div class="text-center mt-3 text-slate-500 text-sm">
+        Built with 💚 by ize-302
+      </div>
+    </div>
   );
 }
 
